29be5d8f340e076fb635f8db1e7cd88da048de92
VickyyStamova <168460505+VickyyStamova@users.noreply.github.com>
2024-05-23 21:29:07 +0300

    angular-w4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TrafficLightComponent } from './traffic-light/traffic-light.component';
+import { EmergancyComponent } from './emergancy/emergancy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TrafficLightComponent, EmergancyComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-w4';
+  title = 'traffic';
 }
